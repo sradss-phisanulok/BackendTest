ab5e78eada69d76d0ba7fa61f4e3746def115126
Jintan29 <99947622+Jintan29@users.noreply.github.com>
2025-03-01 22:38:13 +0700

    added new table

---
+++ models/address.js
@@ -0,0 +1,24 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Address extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Address.init({
+    description: DataTypes.STRING
+  }, {
+    sequelize,
+    freezeTableName: true,
+    modelName: 'Address',
+  });
+  return Address;
+};
